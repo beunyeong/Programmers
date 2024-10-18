757cca79b3c15072d6bbba9764410902e71950e7
beunyeong <oo1409@kakao.com>
2024-10-18 19:14:01 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        
+        if(0 < age && 0 <= 120) {
+        answer = 2022 - age + 1;
+            
+        }
+        
+        return answer;
+    }
+}
