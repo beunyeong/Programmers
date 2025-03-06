ee3117fe8691d9d76fb9e57a814737b5c60f674b
Silver <beunyeong.b@gmail.com>
2025-03-06 22:28:41 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.53 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] sides) {
-        int x = sides[0];
-        int y = sides[1];
-        int z = sides[2];
         
-        if(x+y > z && x+z > y && y+z > x) {
+        // 베열을 정렬하여 가장 긴 변 찾기
+        Arrays.sort(sides);       
+        
+        // 가장 짧은 두 변의 합이 가장 긴 변보다 크면 삼각형 가능
+        if(sides[0] + sides[1] > sides[2]) {
             return 1;
         } else {
             return 2;
