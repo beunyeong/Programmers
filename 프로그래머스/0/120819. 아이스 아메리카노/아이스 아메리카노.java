014e28f7f4fece6e11f9c7d58743b53e0c11e2f6
Silver <beunyeong.b@gmail.com>
2025-03-06 20:26:27 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,6 +1,13 @@
 class Solution {
-    public int[] solution(int money) {  
-        
-        return new int[] {money / 5500, money % 5500};
+    public int[] solution(int money) {
+        int[] answer = new int[2]; // 크기가 2인 배열 선언
+
+        int count = money / 5500;  // 구매할 수 있는 아메리카노 개수
+        int change = money % 5500; // 남은 거스름돈
+
+        answer[0] = count; // 배열에 값 저장
+        answer[1] = change; 
+
+        return answer; // 최종 결과 반환
     }
 }
